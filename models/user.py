f76e0acb853571b2e8868dffe5261a6b6917bdb6
m1016m <m1016m@gmail.com>
2023-08-25 12:35:40 +0800

    Add files via upload

--- models/user.py
+++ models/user.py
@@ -2,7 +2,7 @@
 from sqlalchemy import Column, DateTime, String, func
 from sqlalchemy.orm import relationship
 from database import Base
-from sqlalchemy.orm import relationship
+
 
 class Users(Base):
     __tablename__ = 'users'
@@ -12,7 +12,7 @@ class Users(Base):
     image_url = Column(String(length=256)) #line用户头贴
     created_time = Column(DateTime, default=func.now()) #line用户被建立 
 
-    orders = relationship('Orders', backref='user')#加上這行建立訂單關聯性
+    #orders = relationship('Orders', backref='user')#加上這行建立訂單關聯性
 
     # user.orders 未來只要用這個指令就可以知道user所有的訂單
     # [<Order 1>, <Order 2>]
